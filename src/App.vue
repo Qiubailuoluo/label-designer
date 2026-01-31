86c1dc4d6157ffd3a6b7dd2d2c9d884ead059f82
HuYingjie <huyingjie@cndatacom.com>
2026-01-31 19:14:24 +0800

    V1.0 空界面清理+前置配置文件

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>标签设计器项目</h1>
+    <p>项目已启动，等待产品原型确定...</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup lang="ts">
+// 空脚本 - 后续添加
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+h1 {
+  color: #333;
 }
 </style>
